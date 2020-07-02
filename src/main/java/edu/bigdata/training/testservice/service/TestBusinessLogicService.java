e584bdc537ba21e217f9dc6480659256c27e048f
VDSevch <vladokyokydoky@mail.com>
2020-07-03 01:27:07 +0400

    First commit

---
+++ src/main/java/edu/bigdata/training/testservice/service/TestBusinessLogicService.java
@@ -0,0 +1,31 @@
+package main.java.edu.bigdata.training.testservice.service;
+
+import edu.bigdata.training.testservice.controller.model.Person;
+import edu.bigdata.training.testservice.dao.TestServiceRepository;
+import edu.bigdata.training.testservice.model.PersonEntity;
+
+import java.util.List;
+import java.util.UUID;
+
+public class TestBusinessLogicService {
+
+    private TestServiceRepository testServiceRepository;
+
+    public TestBusinessLogicService(TestServiceRepository testServiceRepository) {
+        this.testServiceRepository = testServiceRepository;
+    }
+
+    public PersonEntity processCreate(Person person){
+        PersonEntity personEntity = new PersonEntity(person.getName());
+        testServiceRepository.save(personEntity);
+        return personEntity;
+    }
+
+    public PersonEntity processGet(String id){
+        return testServiceRepository.get(UUID.fromString(id));
+    }
+
+    public List<PersonEntity> processGetAll(){
+        return testServiceRepository.getAll();
+    }
+}
